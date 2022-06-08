478796d7f4db3a70d7272113a36235d904f88cf0
trezi29 <trezi29@gmail.com>
2022-06-08 12:39:27 +0200

    Adds Javascript

---
+++ index.js
@@ -0,0 +1,27 @@
+let display = document.getElementById('calc_screen_lcd');
+
+let buttons = Array.from(document.getElementsByClassName('calc_key_int'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '←':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
